a62dc7d56005585215d220a2275268f32a402f7f
PrabhavSwaminathan <42639975+PrabhavSwaminathan@users.noreply.github.com>
2021-07-06 14:39:19 +1000

    Update name-sorter.cs

--- name-sorter.cs
+++ name-sorter.cs
@@ -9,42 +9,51 @@ namespace GlobalXSort
     {
         public static void Main(string[] args)
         {
+            //checks if no argument, or more than one argument {text file with names} is available 
             if (args == null || args.Length == 0 || args.Length > 1)
-                {
-                    Console.WriteLine("Please specify, not more than one, filename as a parameter.");
-                    return;
-                }
-
+            {
+                Console.WriteLine("Please specify, not more than one, filename as a parameter.");
+                return;
+            }
+            //Reading all the names stored in the file and storing it in an array
             var logFile = File.ReadAllLines(args[0]);
+            //Saving the names as a list
             var logList = new List<string>(logFile);
-            
+
+            /*
+             * This method uses the LINQ method orderby and thenby to sort the list of names
+             * The orderby method first splits the name of the user using the Split method and sorts by the last name using the .Last() function
+             * Then  it splits the name again and sorts by the first name using the .First() function
+             * It checks the middle name if someone has a middle name and sorts it alphabetically 
+             * Finally it checks the second middle name and sorts it alphabetically
+            */
             List<string> sortedUsers = logList.OrderBy(user => user.Split(" ").Last()).
             ThenBy(user => user.Split(" ").First()).
-            ThenBy(user => user.Split(" ")[1]).
-            ThenBy(user => user.Split(" ").Length == 4 ? user.Split(" ")[2]: "").
+            ThenBy(user => user.Split(" ").Length == 3 ? user.Split(" ")[1] : "").
+            ThenBy(user => user.Split(" ").Length == 4 ? user.Split(" ")[2] : "").
             ToList();
 
             sortedUsers.ForEach(Console.WriteLine);
 
-            //1. Get the name of the output file
+            //The name of the output file
             var fileName = "sorted-names-list.txt";
- 
-            //2. Create the file
+
+            // Creating the file
             var file = new FileStream(fileName, FileMode.OpenOrCreate);
- 
-            //3. Save the standard output
+
+            // Saving the standard output
             var standardOutput = Console.Out;
- 
-            //5. Create a StreamWriter
+
+            // Creating a StreamWriter
             using (var writer = new StreamWriter(file))
             {
-                //6. Set the new output
+                // Setting the new output
                 Console.SetOut(writer);
-                //7. Write something
+                // Writing output to file
                 sortedUsers.ForEach(Console.WriteLine);
-                //8. Change the ouput again
+                // Changing the ouput again
                 Console.SetOut(standardOutput);
             }
         }
     }
-} 
+}
